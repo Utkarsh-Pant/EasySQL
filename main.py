808628fc7047b997d5cc8bad2fe4f41b81cee160
Utkarsh Pant <utkarshpant2005@gmail.com>
2024-11-17 13:52:12 +0530

    Code comment improvements. Reduced varchar size to 255 incase of index constraints

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def notTypeValid(type, text):
         return False
     elif type=="char":
         return len(text) != 0
-    elif type=="varchar(1024)":
+    elif type=="varchar(255)":
         return False
     else: raise ValueError("Not supported type")
     
@@ -52,28 +52,32 @@ def startFunction(initialWindow, startButton: Button):
     subHeading=Label(initialWindow, text="Enter SQL connection details", font=('Terminal', round(screenHeight*screenWidth*0.000018113)))
     subHeading.place(anchor=CENTER, x=round(screenWidth/2), y=screenHeight*0.2)
 
-    dataWidgets = ["Localhost", "Port", "Username", "Password"]
+    dataWidgets = ["Host", "Port", "Username", "Password"]
     defaultValues = ['localhost', '3306', 'root', '']
 
     for i in range(len(dataWidgets)):
+        # Create labels and input entries with default values like , Host: [localhost]
+
         dataLabel = Label(initialWindow, text=dataWidgets[i], font=('Terminal', round(screenHeight*screenWidth*0.0000113), font.BOLD))
         dataLabel.place(anchor=E, relx=0.5, rely=0.3 + i*0.1)
         dataEntry = Entry(initialWindow, width = round((screenWidth/4) * 0.0625), font=('Arial', round(screenHeight*screenWidth*0.0000113), font.BOLD), highlightcolor='black', highlightthickness=1, highlightbackground='black')
         dataEntry.insert(0, defaultValues[i])
         dataEntry.place(anchor=W, relx=0.5, rely = 0.3 + i*0.1)
+        
+        # Overwrite the label and entry to dataWidgets
         dataWidgets[i] = [dataLabel, dataEntry]
 
-    dataWidgets[-1][-1].config(show='*')   #Change password entry display to show stars
+    dataWidgets[-1][-1].config(show='*')  #Change password entry display to show stars
     
-
     errorLabel = Label(initialWindow, text="Invalid Connection Details", fg='red', font=('Terminal', round(screenHeight*screenWidth*0.0000113), font.BOLD))
 
-    def resetDetails(): 
+    def resetDetails(): #self-explanatory :/
         for i in dataWidgets:
             i[1][0].delete(0, END)
             i[1][0].insert(0, defaultValues[dataWidgets.index(i)])
 
     def submitDetails():
+
         try:
             connection = mysql.connector.connect(host=dataWidgets[0][1].get(), port=int(dataWidgets[1][1].get()), user=dataWidgets[2][1].get(), password=dataWidgets[3][1].get())
         except:
@@ -89,9 +93,7 @@ def startFunction(initialWindow, startButton: Button):
 
         submitButton.destroy()
         resetButton.destroy()
-        errorLabel.destroy()
-        
-        
+        errorLabel.destroy()                
         
         dbConnectionMenu(initialWindow, subHeading, connection)
                      
@@ -148,15 +150,16 @@ def dbConnectionMenu(initialWindow, subHeading, connection):
         buttonFrame.destroy()
         createDbButton.destroy()
         deleteDbButton.destroy()
+
         for buttons in allButtons: buttons.destroy()
 
         subHeading.config(text="Enter new database name")
+
         nameEntry = Entry(initialWindow, width = round((screenWidth/4) * 0.0625), font=('Arial', round(screenHeight*screenWidth*0.0000113), font.BOLD), highlightcolor='black', highlightthickness=1, highlightbackground='black')
         nameEntry.place(anchor=CENTER, x=round(screenWidth/2), y=screenHeight*0.5)
 
         submitButton = Button(initialWindow, text="Submit", font=('Terminal', round(screenHeight*screenWidth*0.0000113), font.BOLD), command = lambda: createDbSubmit(nameEntry.get()))
         submitButton.place(anchor=CENTER, x=round(screenWidth/2), y=screenHeight*0.6)
-        pass
 
     def deleteDb(db):
         '''
@@ -185,7 +188,7 @@ def dbConnectionMenu(initialWindow, subHeading, connection):
         Method called when swithcing between delete mode and normal mode
         Changes color of buttons and swaps value of delMode
         '''
-        nonlocal delMode
+        nonlocal delMode #Without nonlocal creates a local scope delMode ;0
         delMode = not delMode
         if delMode == True: 
             for dbButton in allButtons: dbButton['fg']="red"                
@@ -217,12 +220,13 @@ def dbConnectionMenu(initialWindow, subHeading, connection):
 
     subHeading.config(text="Choose Database")
 
+
+    #Creating A scrollable Frame/Canvas which will hold the database buttons
     selectorCanvas = Canvas(initialWindow)
     selectorCanvas.place(anchor=N, relx=0.5, rely=0.3, relwidth=0.5, relheight=0.5)
     selectorScrollbar = Scrollbar(initialWindow, orient="vertical", command=selectorCanvas.yview)
     selectorScrollbar.place(anchor=W, relx=0.75, rely=0.55, relheight=0.5)
     selectorCanvas.configure(yscrollcommand=selectorScrollbar.set)
-    
 
     buttonFrame = Frame(selectorCanvas)
     selectorCanvas.create_window((0,0), window=buttonFrame, anchor="nw")
@@ -235,6 +239,7 @@ def dbConnectionMenu(initialWindow, subHeading, connection):
 
     allButtons = []
     for db in cursor:
+        #Adding the buttons to the frame
         dbButton = Button(buttonFrame, text=db[0], borderwidth=0, background="white", highlightcolor="white", activeforeground="blue", anchor=W, height=round(screenHeight*0.5*0.0075), width=round(screenWidth*0.5*0.9))
         dbButton.configure(command= lambda dbButton = dbButton: connectDB(dbButton) if delMode == False else deleteDb(dbButton))
         dbButton.grid(row=len(allButtons), column=0, pady=0.1)
@@ -252,14 +257,15 @@ def dbConnectionMenu(initialWindow, subHeading, connection):
 
 
 def queryMenu(initialWindow, subHeading, connection):
+
     subHeading.config(text="Select SQL Query")
 
+    #Similarly creating a scrollable Frame/Canvas to hold all the supported query buttons
     selectorCanvas = Canvas(initialWindow)
     selectorCanvas.place(anchor=N, relx=0.5, rely=0.3, relwidth=0.5, relheight=0.5)
     selectorScrollbar = Scrollbar(initialWindow, orient="vertical", command=selectorCanvas.yview)
     selectorScrollbar.place(anchor=W, relx=0.75, rely=0.55, relheight=0.5)
     selectorCanvas.configure(yscrollcommand=selectorScrollbar.set)
-    
 
     buttonFrame = Frame(selectorCanvas)
     selectorCanvas.create_window((0,0), window=buttonFrame, anchor="nw")
@@ -267,6 +273,7 @@ def queryMenu(initialWindow, subHeading, connection):
     supportedQueries = {"Create Table": lambda: createTableMenu(connection), "Insert Data": insertDataMenu, "Select Data": selectDataMenu, "Update Data": updateDataMenu, "Delete Data": deleteDataMenu, "Drop Table": dropTableMenu, "Show Tables": showTablesMenu}    
     i = 0
     for key,value in supportedQueries.items():
+        #Creating and adding the buttons to the frame
         qButton = Button(buttonFrame, text=key, borderwidth=0, background="white", highlightcolor="white", activeforeground="blue", anchor=W, height=round(screenHeight*0.5*0.0075), width=round(screenWidth*0.5*0.9))
         qButton.configure(command= value)
         qButton.grid(row=i, column=0, pady=0.1)
@@ -279,8 +286,22 @@ def queryMenu(initialWindow, subHeading, connection):
 def createTableMenu(connection,tableMenu: Tk = None, columnData: list[list[str]] = []):
 
     connection = mysql.connector.connect(host=connection._host, port=connection._port, user=connection._user, password=connection._password, database=connection._database)
-
+    
     def addTable():
+        '''
+        Function Called when Submit button is clicked
+        Validates all information and executes the query
+        Destroys current window and recalls the createTableMenu function
+
+        Validation Checks:
+        - If table name is empty
+        - If table name is invalid
+        - If table contains no columns
+
+        On success, closes and reopens the createtablemenu
+        '''
+
+
         nonlocal tableNameEntry, columnData, connection
         if len(tableNameEntry.get()) == 0:
             messagebox.showerror("Error", "Table name is required")
@@ -294,51 +315,33 @@ def createTableMenu(connection,tableMenu: Tk = None, columnData: list[list[str]]
         
         query = f"CREATE TABLE {tableNameEntry.get()}("
         for column in columnData:
-            if column[1][3][0] and column[1][0] == "varchar(1024)": column[1][3][1] = '`'+column[1][3][1]+'`'
+            if column[1][3][0] and column[1][0] == "varchar(255)": column[1][3][1] = '`'+column[1][3][1]+'`'
             query+= f"{column[0]} {column[1][0]} "
             query += "NOT NULL " * (column[1][1])
             query += "UNIQUE " * (column[1][2])
             query += f"DEFAULT \'{column[1][3][1]}\'" * column[1][3][0]
             query += "PRIMARY KEY" * (column[1][4])
-            query += ","
-        
+            query += ","   
         query = query[:-1] + ");"
-        print(query)
+        pyperclip.copy(query)
         cursor = connection.cursor(buffered=True)
         cursor.execute(query)
         cursor.close()
         connection.commit()
-        pyperclip.copy(query)
+        
 
         messagebox.showinfo("Table Created", "Table Created Successfully")
+        connection.close()
+        tableMenu.destroy()
+
+        createTableMenu(connection, None, [])
         return 0
 
     def addColumnMenu():
-        def columnAdd():
-            nonlocal tableMenu, columnData, temporaryElements, tableNameEntry, nullToggle, uniqueToggle, defToggle, defEntry, dataTypeComboBox, primaryToggle
-            
-            if uniqueToggle.get() == 1 and defToggle.get() == 1:
-                messagebox.showerror("Error", "Column Cannot Be Unique and Have Default Value")
-                return 0
-
-            if len(tableNameEntry.get()) == 0:
-                messagebox.showerror("Error", "Column Name is required")
-                return 0
-            for columns in columnData:
-                if columns[0] == tableNameEntry.get():
-                    messagebox.showerror("Error", "Column Name Should Be Unique")
-                    return 0
-            if notValidIdentifier(tableNameEntry.get()):
-                messagebox.showerror("Error", "Column Name Invalid")
-                return 0
-            if defToggle.get() and notTypeValid(dataTypeComboBox.get(), defEntry.get()):
-                messagebox.showerror("Error", "Default value invalid")
-                return 0
-            columnData.append([tableNameEntry.get(), [dataTypeComboBox.get(), nullToggle.get(), uniqueToggle.get(), [defToggle.get(), defEntry.get()], primaryToggle.get()]])
-            
-            for element in temporaryElements: element.destroy()
-            createTableMenu(connection, tableMenu, columnData)
-        
+        '''
+        Function Called when Add Column button is clicked
+        Replaces current table with a new temporary table to enter details
+        '''       
 
         nonlocal tableMenu, tableNameLabel, tableNameEntry, displayTable, submitButton, addColButton
 
@@ -348,6 +351,7 @@ def createTableMenu(connection,tableMenu: Tk = None, columnData: list[list[str]]
         temporaryElements = [tableNameLabel, tableNameEntry]
 
         tableNameLabel.config(text="Column Name:")
+
 
         nullToggle = IntVar(tableMenu)
         nullCheck = ttk.Checkbutton(tableMenu, variable = nullToggle)
@@ -380,10 +384,50 @@ def createTableMenu(connection,tableMenu: Tk = None, columnData: list[list[str]]
 
         dataTypeLabel = Label(tableMenu, text="Data Type:", font=('Terminal', round(screenHeight*screenWidth*0.000009113), font.BOLD))
         dataTypeLabel.place(anchor = E, relx=0.5, rely=0.7)
-        dataTypeComboBox =  ttk.Combobox(tableMenu, values = ("int","decimal","date","varchar(1024)","char"), state="readonly")
+        dataTypeComboBox =  ttk.Combobox(tableMenu, values = ("int","decimal","date","varchar(255)","char"), state="readonly")
         dataTypeComboBox.place(anchor= W, relx=0.5, rely= 0.7)
         dataTypeComboBox.current(0)
         temporaryElements.extend([dataTypeLabel, dataTypeComboBox])
+
+        def columnAdd():
+            '''
+            Function Called when Submit button is clicked in the add column menu
+            Validates all information and appends the additional data to columnData
+            Destroys current window and recalls the createTableMenu function
+
+            Validation Checks:
+            - If it contains both unique and default values
+            - If column name is not present
+            - If column name is already in columns
+            - If column name is invalid
+            - If default value is invalid w.r.t data type
+            '''
+
+            nonlocal tableMenu, columnData
+            
+            if uniqueToggle.get() == 1 and defToggle.get() == 1:
+                messagebox.showerror("Error", "Column Cannot Be Unique and Have Default Value")
+                return 0
+
+            if len(tableNameEntry.get()) == 0:
+                messagebox.showerror("Error", "Column Name is required")
+                return 0
+            for columns in columnData:
+                if columns[0] == tableNameEntry.get():
+                    messagebox.showerror("Error", "Column Name Should Be Unique")
+                    return 0
+            if notValidIdentifier(tableNameEntry.get()):
+                messagebox.showerror("Error", "Column Name Invalid")
+                return 0
+            if defToggle.get() and notTypeValid(dataTypeComboBox.get(), defEntry.get()):
+                messagebox.showerror("Error", "Default value invalid")
+                return 0
+            
+
+            columnData.append([tableNameEntry.get(), [dataTypeComboBox.get(), nullToggle.get(), uniqueToggle.get(), [defToggle.get(), defEntry.get()], primaryToggle.get()]])
+            for element in temporaryElements: element.destroy()
+
+            createTableMenu(connection, tableMenu, columnData)
 
         addColButton.place_forget()
         addColButton.place(anchor=CENTER, relx=0.5, rely= 0.85)
@@ -404,23 +448,28 @@ def createTableMenu(connection,tableMenu: Tk = None, columnData: list[list[str]]
 
     displayTable = ttk.Treeview(tableMenu)
     displayTable['columns'] = ('Column', 'Attributes')
+
     displayTable.column("#0", minwidth=10)
-    displayTable.column("Column", minwidth = 10)
+    displayTable.column("Column", minwidth = 5)
     displayTable.column("Attributes", minwidth = 10)
+
     displayTable.heading("#0", text="S. No.", anchor = W)        
     displayTable.heading("Column", text="Column", anchor = W)
-    displayTable.heading("Attributes", text="Attributes", anchor = W)        
+    displayTable.heading("Attributes", text="Attributes", anchor = W)  
+
     displayTable.place(anchor=N, relx = 0.5, rely = 0.3, width = screenWidth/2.5)
 
-    print(columnData)
+
     for i in range(len(columnData)):
-        
+        #Adding the columns to the table along with the attributes
+
         attributeList = columnData[i][1]
         attributeStr = f"DataType: {attributeList[0]}"
         if attributeList[1] == 1: attributeStr += ", No Null Values"
         if attributeList[2] == 1: attributeStr += ", Unique Values Only"
         if attributeList[3][0] == 1: attributeStr += f", Default Value {attributeList[3][1]}"
         if attributeList[4] == 1: attributeStr += ", Primary Key"
+        
         displayTable.insert(parent="", index='end', iid = i, text=i, values = (columnData[i][0], attributeStr))
 
     submitButton = Button(tableMenu, text="Submit", font=('Terminal', round(screenHeight*screenWidth*0.0000113), font.BOLD), command=addTable)
